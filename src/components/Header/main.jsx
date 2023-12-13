cdabf29552c5f654a4fa81d630c80705300cf445
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-12-13 19:56:08 -0300

    remove div

--- src/components/Header/main.jsx
+++ src/components/Header/main.jsx
@@ -1,14 +1,9 @@
-import React from "react"
-
 export default function Header() {
   return (
     <>
-      <div className="mx-2">
+      <div className="px-2 bg-white shadow-lg ">
         <div className="p-3 text-3xl  flex justify-between">
           <h1>Logo</h1>
-          <div className="flex gap-2">
-            <h1>Usado</h1>|<h1>Novo</h1>
-          </div>
           <h1>Login</h1>
         </div>
       </div>
