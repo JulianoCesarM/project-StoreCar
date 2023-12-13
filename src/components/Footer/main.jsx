809a85dbc488e4740d25b78ee6c025c4a6c593b5
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-12-13 19:55:43 -0300

    added logo tailwind and adding style

--- src/components/Footer/main.jsx
+++ src/components/Footer/main.jsx
@@ -1,24 +1,33 @@
-import React from "react"
 import viteLogo from "/vite.svg"
 import reactLogo from "/react.svg"
+import tailwind from "/tailwind.svg"
+
 export default function Footer() {
   return (
     <>
-      <div className="m-8 bottom-0">
-        <div className="flex justify-center flex-col">
-          <h2>Tecnologias</h2>
-          <div className="text-1xl font-bold underline flex gap-8">
+      <div className="mt-5 py-6 px-4 bg-blue-100 ">
+        <div className=" w-80 m-auto ">
+          <h2 className="text-2xl mb-6 text-center">Tecnologias usadas</h2>
+          <div className="flex justify-between items-center font-bold underline flex gap-8">
             <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
+              <img
+                src={viteLogo}
+                className="logo vite bg-cover w-40"
+                alt="Vite logo"
+              />
             </a>
             <a href="https://react.dev" target="_blank" rel="noreferrer">
-              <img src={reactLogo} className="logo react" alt="React logo" />
+              <img
+                src={reactLogo}
+                className="logo react w-44"
+                alt="React logo"
+              />
             </a>
             <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
               <img
-                src={reactLogo}
-                className="logo tailwind"
-                alt="logo tailwind"
+                src={tailwind}
+                className="logo tailwind w-50"
+                alt="tailwind logo"
               />
             </a>
           </div>
