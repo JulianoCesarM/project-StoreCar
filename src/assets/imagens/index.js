e9a0a07057758f443465c7974fd73aa598ee72d2
Juliano Cesar Magoga <julianoc.m@hotmail.com>
2023-12-14 18:22:15 -0300

    adding images and code to export the images

---
+++ src/assets/imagens/index.js
@@ -0,0 +1,3 @@
+export { default as image1 } from "../../../public/background/image1.jpg"
+export { default as image2 } from "../../../public/background/image2.jpg"
+export { default as image3 } from "../../../public/background/image3.jpg"
