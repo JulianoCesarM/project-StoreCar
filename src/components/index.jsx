89e39d62c9ff60f770abb8635ac64e5a52b58fbb
JulianoC <julianoc.m@hotmail.com>
2024-07-13 15:09:03 -0300

    comentario da alteração da estrutura do projeto

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,6 +4,9 @@ import UsedCars from "./Cars/Usados"
 export default function BodyContent() {
   return (
     <>
+      {/* Ficara um switch que mudara a "pagina" de novo,usado e ofertas 
+      e 
+      será adicionado uma page banner onde ira passar imagens das logos do veiculos*/}
       <div className="blur-bgs flex flex-col gap-10 bg-white">
         <NewCars />
         <UsedCars />
