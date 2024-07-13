3337bce41fc5472e2169fa1913e971e7e22940ff
JulianoC <julianoc.m@hotmail.com>
2024-07-13 15:58:23 -0300

    encurtando o caminho do link

--- src/assets/imagens/index.js
+++ src/assets/imagens/index.js
@@ -1,3 +1,3 @@
-export { default as AntonMartin } from "../../../public/ImageBannerLogo/Aston-Martin-Logo-1984.png"
-export { default as Ferrari } from "../../../public/ImageBannerLogo/Ferrari-Logo-2010.png"
-export { default as Porshe } from "../../../public/ImageBannerLogo/Porsche-Logo.png"
+export { default as AntonMartin } from "/ImageBannerLogo/Aston-Martin-Logo-1984.png"
+export { default as Ferrari } from "/ImageBannerLogo/Ferrari-Logo-2010.png"
+export { default as Porshe } from "/ImageBannerLogo/Porsche-Logo.png"
