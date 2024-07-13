562e804af0d0fde72ce902592f2843c4599db3c4
JulianoC <julianoc.m@hotmail.com>
2024-07-13 15:41:51 -0300

    corigindo nome da pasta onde imagens de banner irao ficar e corrigindo o link que puxa as imagens

--- src/assets/imagens/index.js
+++ src/assets/imagens/index.js
@@ -1,3 +1,3 @@
-export { default as AntonMartin } from "../../../public/background/Aston-Martin-Logo-1984.png"
-export { default as Ferrari } from "../../../public/background/Ferrari-Logo-2010.png"
-export { default as Porshe } from "../../../public/background/Porsche-Logo.png"
+export { default as AntonMartin } from "../../../public/ImageBannerLogo/Aston-Martin-Logo-1984.png"
+export { default as Ferrari } from "../../../public/ImageBannerLogo/Ferrari-Logo-2010.png"
+export { default as Porshe } from "../../../public/ImageBannerLogo/Porsche-Logo.png"
